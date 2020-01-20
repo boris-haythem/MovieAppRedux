1d20a3fb7b106600a6bac66183dfd9201e30e4d9
boris-haythem <haythem.gharrad@gmail.com>
2020-01-20 09:36:31 +0100

    movieappredux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Movie from './components/movieslist'
+function App() {
+  return (
+    <div className="App">
+      <Movie/> 
+    </div>
+  );
+}
+
+export default App;
